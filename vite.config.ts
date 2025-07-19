9e10d3729078c726b4ae298b7539eb64e52b4f79
andrii <zharkov.php@gmail.com>
2025-07-19 11:33:00 -0600

    add server config port 5173 for vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
